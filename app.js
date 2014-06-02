6a095e8f9b8f42120f897c28f8267af1432eabe1
teroniemisto <tero.niemisto@digia.com>
2014-06-02 13:58:22 +0300

    Added start script

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
     console.log('Listening on port %d', server.address().port);
 });
